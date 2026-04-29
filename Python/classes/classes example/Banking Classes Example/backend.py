d0283650893eed2f5e718d081e27527d33e94773
Nexus <timotej.petrik1@gmail.com>
2026-04-29 12:27:57 +0200

    Update backend.py

--- Python/classes/classes example/Banking Classes Example/backend.py
+++ Python/classes/classes example/Banking Classes Example/backend.py
@@ -9,11 +9,11 @@ class BankAccount:
         self.balance = 0
 
 
-    def deposit(self, amount):
+    def Deposit(self, amount):
         self.balance += amount
         print(f"{self.name} deposited ${amount}, Current balance: ${self.balance}")
 
-    def withdraw(self, amount):
+    def Withdraw(self, amount):
 
         if self.balance - amount < 0:
             answer = input("You are going into negative balance, this will lead to fees, continue? y/n ")
@@ -28,6 +28,10 @@ class BankAccount:
             self.balance -= amount
             print(f"{self.name} withdrew ${amount}, Current balance: ${self.balance}")
 
+    def ChangeAccountName():
+        name = input("Please enter a new name: ")
+        self.name = name
+        
 
 
 
